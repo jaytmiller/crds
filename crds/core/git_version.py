c97c90ac1d1e895c515d7fdefba76980626c4581
Todd Miller <jmiller@stsci.edu>
2019-11-07 11:35:47 -0500

    Updated version info to 7.4.1.3

--- crds/core/git_version.py
+++ crds/core/git_version.py
@@ -1,9 +1,9 @@
 
 
-__version__ = '010d1ed677989f211cf25148704b9090fe1788b2'
+__version__ = 'f132cbddf56d4bd21ab22996cadcc90fa99105c3'
 
 __full_version_info__ = '''
-branch: b7.4.1.2
-sha1: 010d1ed677989f211cf25148704b9090fe1788b2
+branch: b7.4.1.3
+sha1: f132cbddf56d4bd21ab22996cadcc90fa99105c3
 '''
     
